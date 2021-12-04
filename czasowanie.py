bd1f9a67a58a2a5d83a5b492a2e8641fbeb7ddbb
joohnes <kubapienkowski@gmail.com>
2021-12-04 23:49:39 +0100

    fixed time format

--- czasowanie.py
+++ czasowanie.py
@@ -11,8 +11,8 @@ def plotowanie(amount):
         return num/100
 
 
-    lst = ['Antek', 'Bochyn', 'Hadziu', 'Julia', 'Karolina', 'Pieniek', 'Jezus', 'Gorszy', 'Gontarek', 'Jaron', \
-                'Staniak', 'Sylwia', 'Tomek', 'Wiktor']
+    lst = ['Antek', 'Bochyn', 'Hadziu', 'Karolina', 'Pieniek', 'Jezus', 'Gorszy', 'Gontarek', 'Jaron', \
+            'Staniak', 'Sylwia', 'Tomek', 'Wiktor', "Aleks"]
 
     wyniki = {}
     for x in lst:
@@ -22,14 +22,19 @@ def plotowanie(amount):
             temp[y] = 0
         wyniki[x] = temp
         
-    start_time = f"{localtime().tm_hour}:{localtime().tm_min:02d}:{localtime().tm_sec}"
+    start_time = f"{localtime().tm_hour:02d}:{localtime().tm_min:02d}:{localtime().tm_sec:02d}"
     czas = perf_counter()
     for _ in range(amount):
         wynik = losowanie()
         for x in wynik:
             wyniki[x][wynik[x]] += 1
+            
     czas = perf_counter() - czas    
-
+    temp = int(czas / 3600)
+    temp1 = int((czas - temp*3600) / 60)
+    temp2 = czas - temp * 3600 - temp1 * 60
+    czas = f"{f'{temp:02d}:' if temp > 0 else ''}{f'{temp1:02d}:' if temp1 > 0 else ''}{temp2:.02f}"
+    
     occurences = []
     for x in wyniki:
         for y in wyniki[y]:
@@ -58,23 +63,23 @@ def plotowanie(amount):
   
     
     content = "Predictions:\n"
-    # left = [x for x in predictions]
-    # for x in predictions:
-    #     maxnumber = x
-    #     for y in left:
-    #         if predictions[y][1] > predictions[x][1]:
-    #             maxnumber = y
-    #     left.remove(maxnumber)
-    #     content += f"{maxnumber} - {predictions[maxnumber][0]} -- {(predictions[maxnumber][1]/amount * 100):.2f}%\n" 
+    left = [x for x in predictions]
+    for x in predictions:
+        maxnumber = x
+        for y in left:
+            if predictions[y][1] > predictions[x][1]:
+                maxnumber = y
+        left.remove(maxnumber)
+        content += f"{maxnumber} - {predictions[maxnumber][0]} -- {(predictions[maxnumber][1]/amount * 100):.2f}%\n" 
     for x in predictions:
         content += f"{x} - {predictions[x][0]} - {(predictions[x][1]/amount * 100):.2f}%\n"
     content += '\n\n'
     for x in occurences:
         content += f"{x[1]} - {x[0]} -- {(x[0]/amount * 100):.2f}%\n"
         
-    sendMail(Subject=f"Done in {czas:.02f}s, started at {start_time}", content=content, file= "results.png")
+    sendMail(Subject=f"Done in {czas}s, started at {start_time}", content=content, file= "results.png")
 
 if __name__ == '__main__':
-    AMOUNT = 100
+    AMOUNT = 50000
     
     plotowanie(AMOUNT)
